66cd363fd110cd138067fa2a9876211335be7a3f
HOED <hoed@pict.edu>
2019-03-23 13:13:57 +0530

    VueJS Basics from Documentation, Scrimba

---
+++ VueJSWebApp1/index.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message:'Hello Kshitij'
+    }
+});
+
+app.message = 'I have changed the Data'
---
+++ VueJSWebApp7/abc.py
@@ -0,0 +1,6 @@
+js = open("index.js","w")
+html = open("index.html","w")
+css = open("index.css","w")
+
+js.write("var app = new Vue();")
+
